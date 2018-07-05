5d8248402a0e710b7dc358206036b9d7b333da28
ejobe <ejo@uchicago.edu>
2018-07-05 14:30:39 -0500

    initial addition of ping-pong acquisition mode

--- evm.py
+++ evm.py
@@ -6,6 +6,9 @@ import usb_device as usbdev
 import registers as reg
 import psec4a
 
+'''EJO 2018.2.1
+'''
+
 class EVM:
 
    ext_dac_ref_voltage = 1.190 #volts
@@ -20,6 +23,8 @@ class EVM:
       self.dev.set_configuration()
       self.dev.reset()
       self.psec4a = psec4a.PSEC4A(self)
+      self.mode = 0
+      self.getReadoutMode()
 
       self.pedestals = numpy.zeros((8,1056))
 
@@ -41,41 +46,58 @@ class EVM:
       ret = self.dev.read(usbdev.EDPNT_RD, num_bytes)
 
       if debug:
-         print "register read:"
-         for i in range(0,len(ret),-1):
-            print "word recieved: ", hex((ret[i+1] << 8) | ret[i])
+         print "register read:", ret
+         #for i in range(0,len(ret),-1):
+         #   print "word recieved: ", hex((ret[i+1] << 8) | ret[i])
 
       return (ret[3] << 8) | ret[2]
 
    def readChannel(self, channel, num_bytes = 2120 ):
       '''read single PSEC4A channel
       '''
+
       self.writeRegister(reg.map['READ_CHAN'], channel)
       self.dev.write(usbdev.EDPNT_WR, [0x00, 0x00, 0x00, reg.map['READ_DATA_REG']])
       ret = self.dev.read(usbdev.EDPNT_RD, num_bytes)
 
       offset = 6 #bytes
       length= self.psec4a_samples #channel samples
-      data = numpy.bitwise_or(ret[offset:offset+length*2:2], numpy.left_shift(ret[offset+1:offset+length*2+1:2], 8))
+      data = numpy.bitwise_or(ret[offset:offset+length*2:2], numpy.left_shift(numpy.bitwise_and(ret[offset+1:offset+length*2+1:2], 7) , 8))
+      buf = numpy.right_shift(numpy.bitwise_and(ret[offset+1:offset+length*2+1:2], 16), 4) #psec4a readout block no. embedded in data
 
-      return data
+      return data, buf[0]
 
+   ###########  
    def readEvent(self, pedestal_sub=False):
       '''read full 8 channel event
          returns numpy array
       '''
       data=[]
-      for i in xrange(8):
-         data.append(self.readChannel(i+1))
 
-         for j in xrange(8):
+      #check readout mode to determine number of readout blocks
+      if self.mode == 1:
+         blocks = 4
+         bytes_to_read = 1064 
+      else:
+         blocks = 8
+         bytes_to_read = 2120
+
+      for i in xrange(8):
+         ch, buf = self.readChannel(i+1, bytes_to_read)
+         data.append(ch)
+
+         for j in xrange(blocks):
             data[i][132*j:132*j+132] = numpy.flip(data[i][132*j:132*j+132], 0)
 
       if pedestal_sub:
-         return numpy.array(data) - self.pedestals
+         if self.mode == 1:
+            return numpy.array(data) - self.pedestals[:, (int(buf)*self.psec4a_samples/2):((int(buf)+1)*self.psec4a_samples/2)], buf
+         else:
+            return numpy.array(data) - self.pedestals
       else:
-         return numpy.array(data)
+         return numpy.array(data), buf
 
+   ###########
    def identify(self):
       '''returns board ID info
       '''
@@ -88,12 +110,32 @@ class EVM:
       print 'firmware date:', firm_year, '/', (firm_date & 0xFF00) >> 8, '/', firm_date & 0xFF
       print 
 
+   ###########
    def resetReadout(self):
       self.writeRegister(121, 0x1)
 
+   ###########
+   def getReadoutMode(self):
+      '''readback psec4a mode
+      '''
+      value = self.readRegister(0x4D)
+      self.mode = value
+      return value
+
+   ###########
+   def setReadoutMode(self, mode):
+      '''specify readout mode. 0=readout all samples, 1=ping-pong 528 samples
+      '''
+      self.writeRegister(0x4D, 0x1 & mode)
+      self.mode = self.getReadoutMode()
+   
+   ###########
    def softwareTrigger(self):
+      ''' send software trigger
+      '''
       self.writeRegister(reg.map['SW_TRIG'], 0x1)
 
+   ###########
    def setPedestalVoltage(self, volts=0.60):
       '''
       sets dc offset 'pedestal' voltage on psec4a input RF
@@ -102,6 +144,7 @@ class EVM:
       value = int(volts / self.ext_dac_ref_voltage * 0xFFFF)
       self.writeRegister(reg.map['PED_VOLTS'], value)
 
+   ###########
    def readPedestalVoltage(self):
       '''read value of pedestal voltage register
       '''
@@ -109,28 +152,47 @@ class EVM:
       volts = float(value) / 0xFFFF * self.ext_dac_ref_voltage
       return volts
 
-
    ###########
    def takePedestals(self, num=100, filename='pedestals.dat'):
       '''get pedestal offset values for each storage cell
       '''
       self.resetReadout()
+
       pedestals = numpy.zeros((self.psec4a_channels,self.psec4a_samples))
       
       for j in range(num):
-         self.softwareTrigger()
-         pedestals = pedestals + self.readEvent(pedestal_sub=False)
+         
+         #if reading out in pingpong mode, need to record data for each block
+         if self.mode == 1:
+            for i in range(2):
+               self.softwareTrigger()
+               temp = self.readEvent(pedestal_sub=False)
+               pedestals[:, (int(temp[1])*self.psec4a_samples/2):((int(temp[1])+1)*self.psec4a_samples/2)] = \
+                                 pedestals[:, (int(temp[1])*self.psec4a_samples/2):((int(temp[1])+1)*self.psec4a_samples/2)] + temp[0]
+
+
+               #print i, pedestals[:, (int(temp[1])*self.psec4a_samples/2):((int(temp[1])+1)*self.psec4a_samples/2)].shape
+
+         #otherwise, reading out whole window:
+         else:
+            self.softwareTrigger()
+
+            temp = self.readEvent(pedestal_sub=False)
+            pedestals = pedestals + temp[0]
       
+
       self.pedestals = pedestals / num
 
       if filename is not None:
          numpy.savetxt(filename, self.pedestals, delimiter='\t')
 
       return self.pedestals
-
+   
+   ###########
    def loadPedestals(self, filename='pedestals.dat'):
+      '''load pedestal data from file
+      '''
       self.pedestals = numpy.loadtxt(filename)
-
 
    ###########
    def pedestalScan(self, start_volts=0.00, stop_volts=1.20, incr_volts=0.05, filename='pedscan.dat'):
@@ -144,7 +206,7 @@ class EVM:
       for i, ped_voltage in enumerate(ped_voltages):
 
          self.setPedestalVoltage(ped_voltage)
-         time.sleep(1.5)
+         time.sleep(1.5) #let DAC voltage settle
          set_voltage = self.readPedestalVoltage()
          
          sys.stdout.write('pedestal voltage is.....{:.2f} V\r'.format(set_voltage))
@@ -165,6 +227,7 @@ class EVM:
       return ped_voltages, scan_values
 
 
+#--------------------
 if __name__=='__main__':
    dev = EVM()
    dev.identify()
