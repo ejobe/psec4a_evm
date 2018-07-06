2cd779f3d9f296c5d6cbed5759057283de845d9e
ejobe <ejo@uchicago.edu>
2018-07-06 18:15:58 -0500

    add count-to-voltage calc. and data logger

---
+++ log_data.py
@@ -0,0 +1,56 @@
+import evm
+import numpy
+import h5py
+import sys
+import time
+
+def data_logger(nevents, sw_trig=True, outfile=None, ped_subtract=False):
+    
+    nevents = int(nevents)
+
+    dev = evm.EVM()
+    dev.loadPedestals()
+
+    data=[]
+    buf=[]
+
+    for i in range(nevents):
+
+        if sw_trig:
+            dev.softwareTrigger()
+            
+        time.sleep(0.05) #limit rate
+
+        temp = dev.readEvent(ped_subtract)
+
+        data.append(temp[0])
+        buf.append(temp[1])            
+
+        if (i+1)%10==0:
+            sys.stdout.write('logging event...{:}\r'.format(i+1))
+            sys.stdout.flush()
+        
+    
+    if outfile is not None:
+        
+        f = h5py.File(outfile+'.hd5', 'w')
+
+        _data = f.create_dataset("data", (nevents, 8, 528), dtype='i', data=numpy.array(data), compression="gzip")
+        _block=f.create_dataset("block", (nevents,), dtype='i', data=numpy.array(buf))
+        _ped = f.create_dataset("ped", (8, 1056), dtype='i', data=dev.pedestals)
+
+    print
+
+    return data
+
+
+if __name__=='__main__':
+
+    import matplotlib.pyplot as plt
+
+    data = data_logger(100, outfile='test', ped_subtract=True)
+    
+    for i in range(100):
+        plt.figure(1)
+        plt.plot(data[i][5])
+    plt.show()
--- ped_scan.py
+++ ped_scan.py
@@ -4,37 +4,25 @@ import time
 import matplotlib.pyplot as plt
 
 dev=evm.EVM()
-
-#for i in range(300, 801, 100):
-#    print i
-#    dev.psec4a.setXferBufBias(i)
-#    time.sleep(1)
-dev.psec4a.setXferResetMode(1)
-dev.psec4a.setComparatorBias(0x140) #(0x200)
-dev.psec4a.setXferBufBias(0x120) #(0x120)
-dev.psec4a.setRampCurrent(0x230)        #(0x1EF)
-dev.psec4a.setRampBufBias(0x150)
-dev.psec4a.setXferResetVoltage(0x1e00) #0x2c00 seems to be minimum for leakage...if reading out both 528 sample banks
 time.sleep(2)
-
-ped, codes = dev.pedestalScan(incr_volts=0.1, filename=None)
-
-#    plt.figure(i)
+ped, codes = dev.pedestalScan(incr_volts=0.1, filename='test.dat')
 
 
-ch = 5 
-plt.figure()
-for j in range(528):
-    plt.plot(ped, numpy.array(codes)[:,ch,j], 'o--')
+ch = 1
+for ch in range(2):
 
-plt.figure()
-for j in range(528,1056,1):
-    plt.plot(ped, numpy.array(codes)[:,ch,j], 'o--')
+    for j in range(528):
+        plt.figure(1)
+        plt.plot(ped, numpy.array(codes)[:,ch,j], 'o--')
 
-plt.figure()
+    for j in range(528,1056,1):
+        plt.figure(2)
+        plt.plot(ped, numpy.array(codes)[:,ch,j], 'o--')
 
-plt.plot(ped, numpy.mean(numpy.array(codes)[:,ch,:528], axis=1), 'o--')
-plt.plot(ped, numpy.mean(numpy.array(codes)[:,ch,528:], axis=1), 'o--')
+for ch in range(8):
+    plt.figure(4)
+    plt.plot(ped, numpy.mean(numpy.array(codes)[:,ch,:528], axis=1), 'o--')
+    plt.plot(ped, numpy.mean(numpy.array(codes)[:,ch,528:], axis=1), 'o--')
 
 
 plt.show()
